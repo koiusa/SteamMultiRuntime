2de25a59f0ac5cabb39a9a11654f2475d9bbb6d6
koiusa <koiusa@users.noreply.github.com>
2026-06-02 16:33:29 +0900

    プロパティ表示エディタツールも移動

--- Assets/SteamMultiRuntime/Editor/UnitPropertyDrawer.cs
+++ Assets/SteamMultiRuntime/Editor/UnitPropertyDrawer.cs
@@ -9,26 +9,22 @@ namespace Koiusa.SteamMultiRuntime
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             var unitAttribute = attribute as UnitAttribute;
-            
-            // 単位情報を表示テキストに追加
-            string displayLabel = label.text;
+
+            var displayLabel = label.text;
             if (!string.IsNullOrEmpty(unitAttribute.Unit))
             {
                 displayLabel += $" ({unitAttribute.Unit})";
             }
 
-            // ツールチップに説明を追加
-            string tooltip = label.tooltip;
+            var tooltip = label.tooltip;
             if (!string.IsNullOrEmpty(unitAttribute.Description))
             {
-                tooltip = string.IsNullOrEmpty(tooltip) 
-                    ? unitAttribute.Description 
+                tooltip = string.IsNullOrEmpty(tooltip)
+                    ? unitAttribute.Description
                     : $"{tooltip}\n{unitAttribute.Description}";
             }
 
             var newLabel = new GUIContent(displayLabel, tooltip);
-
-            // プロパティを表示
             EditorGUI.PropertyField(position, property, newLabel);
         }
     }
