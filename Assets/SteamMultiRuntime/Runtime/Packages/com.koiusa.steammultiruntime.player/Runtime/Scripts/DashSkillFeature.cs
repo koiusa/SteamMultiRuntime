32de330b324ae6074b38ada89d117aa21b0ec4fd
koiusa <koiusa@users.noreply.github.com>
2026-07-29 02:55:30 +0900

    ダッシュの進行方向

--- Assets/SteamMultiRuntime/Runtime/Packages/com.koiusa.steammultiruntime.player/Runtime/Scripts/DashSkillFeature.cs
+++ Assets/SteamMultiRuntime/Runtime/Packages/com.koiusa.steammultiruntime.player/Runtime/Scripts/DashSkillFeature.cs
@@ -10,14 +10,26 @@ namespace Koiusa.SteamMultiRuntime
         [SerializeField] private bool useFacingWhenDirectionIsEmpty = true;
 
         private IPlayerMotorMotionSink motor;
+        private IPlayerLocomotionState locomotionState;
         protected override float ActiveDuration => duration;
 
-        private void Awake() => motor = GetComponent<IPlayerMotorMotionSink>();
+        private void Awake()
+        {
+            motor = GetComponent<IPlayerMotorMotionSink>();
+            locomotionState = GetComponent<IPlayerLocomotionState>();
+        }
 
         protected override bool OnActivate(PlayerSkillContext context)
         {
             if (motor == null) motor = GetComponent<IPlayerMotorMotionSink>();
             var direction = context.Direction;
+            if (locomotionState == null) locomotionState = GetComponent<IPlayerLocomotionState>();
+            if (locomotionState != null
+                && locomotionState.IsStrafeMode
+                && locomotionState.MoveDirection.sqrMagnitude > 0.0001f)
+            {
+                direction = locomotionState.MoveDirection;
+            }
             if (direction.sqrMagnitude <= 0.0001f && useFacingWhenDirectionIsEmpty) direction = transform.forward;
             return motor != null && motor.TryStartMotion(
                 new PlayerMotorMotionRequest(GetInstanceID(), direction, speed, duration));
